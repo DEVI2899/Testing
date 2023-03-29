51637e0a81a35d650f3a6cb75c24f2f86fdae254
op0318 <iamthiru9818@gmail.com>
2023-03-29 15:29:17 +0530

    Added the welcome code page!

---
+++ lib/welcomepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Welcomepage extends StatelessWidget {
+  const Welcomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to the dashboard  page',style: TextStyle(fontSize: 30,
+            fontWeight: FontWeight.bold),)          ],
+        ),
+      ),
+    );
+  }
+}
